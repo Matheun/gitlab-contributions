25521d17cac15a79659cf1ef1ecd73887d6a13de
Matheun Struik <matheun@ic10.nl>
2022-03-03 10:28:55 +0000

    add code in files types: .vue,.js,.php,.scss remove code in files types: .vue,.js,.php

--- content.js
+++ content.js
@@ -1,2 +1,2 @@
 console.log("ynchx")
-console.log("esxqx")
+console.log("twyrt")
--- content.php
+++ content.php
@@ -2,5 +2,5 @@
         echo "hdqog";
         echo "aoahp";
         echo "myhnj";
-        echo "asxeo";
+        echo "mlxtq";
 ?>
